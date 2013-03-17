dd2049947764d87fdcc943f9d3d2e5e01710de7e
Alexa Cain <AlexaNCain@gmail.com>
2013-03-16 22:40:44 -0400

    Updated Contender class
    Contenders march to the middle of the battlefield and then fight. Long
    term, they should not know where the middle is, but for proof of concept
    it's working. HP implemented, and base damage function.

--- Contender.js
+++ Contender.js
@@ -3,41 +3,46 @@
 
 function Contender(xLoc, yLoc, imgSrc, sideVal) {
 
-	this.img = imgSrc;
+	this.imgVal = imgSrc;
 	this.x = xLoc;
 	this.y = yLoc;
 	this.side = sideVal;
+	this.alive = true;
 	
 	var sizeVal = Math.floor(Math.random()*1001);
 	if(sizeVal < 800){ 
 		this.size = 1;
 		this.imgSize = 25;
+		this.health = 25;
 	}
 	else if(sizeVal < 950){
 		this.size = 2;
 		this.imgSize = 50;
+		this.health = 40;
 	}
 	else{
 		this.size = 3;
 		this.imgSize = 100;
+		this.health = 75;
 	}	
 }
 
 Contender.prototype.tick = function () {
 	var jitter = Math.floor(Math.random()*1001)
 
-	if(this.side == 1)
-		this.x += 5;
-	else
-		this.x -= 5;
-	
-	if(this.x < 20)
-		this.side = 1;
-	else if(this.x > canvas.width - 120)
-		this.side = 0;
+	if(this.side == 1 && this.x < (canvas.width/2) - 20)
+		this.x += 4;
+	else if(this.side == 0 && this.x > (canvas.width/2))
+		this.x -= 4;
 
 	if(jitter > 950) 
 		this.y += 5;
 	else if( jitter < 50)
 		this.y -= 5;
+}
+
+Contender.prototype.ow = function() {
+	health -= 5;
+	if(health <= 0)
+		this.alive = false;
 }
